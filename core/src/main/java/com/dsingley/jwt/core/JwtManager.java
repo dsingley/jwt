4c8e0e62d9dd8070a89ec9b9902ba69ac1d0911b
David Singley <dsingley@vt.edu>
2025-02-07 10:20:34 -0500

    add token verification predicates for Key ID header and payload claims (#92)

--- core/src/main/java/com/dsingley/jwt/core/JwtManager.java
+++ core/src/main/java/com/dsingley/jwt/core/JwtManager.java
@@ -1,17 +1,21 @@
 package com.dsingley.jwt.core;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.Verification;
 import lombok.Builder;
 import lombok.NonNull;
+import lombok.Singular;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 /**
  * The JwtManager class provides methods for creating and verifying JSON Web Tokens (JWTs).
@@ -21,6 +25,8 @@ import java.util.UUID;
  * <p>
  * Tokens are verified using an {@link Algorithm} provided by the configured {@link VerificationAlgorithmSupplier}
  * and optionally allowing for clock skew if leeway seconds are configured.
+ * <p>
+ * Additional token verification predicates can be configured for the Key ID header and payload claims.
  */
 @Slf4j
 public class JwtManager {
@@ -31,6 +37,8 @@ public class JwtManager {
     private final SigningAlgorithmSupplier signingAlgorithmSupplier;
     private final VerificationAlgorithmSupplier verificationAlgorithmSupplier;
     private final long leewaySeconds;
+    private final Predicate<String> keyIdPredicate;
+    private final Map<String, BiPredicate<Claim, DecodedJWT>> payloadPredicates;
 
     @Builder
     public JwtManager(
@@ -38,13 +46,17 @@ public class JwtManager {
             @NonNull Long ttlSeconds,
             @NonNull SigningAlgorithmSupplier signingAlgorithmSupplier,
             @NonNull VerificationAlgorithmSupplier verificationAlgorithmSupplier,
-            Long leewaySeconds
+            Long leewaySeconds,
+            Predicate<String> keyIdPredicate,
+            @Singular Map<String, BiPredicate<Claim, DecodedJWT>> payloadPredicates
     ) {
         this.issuer = issuer;
         this.ttlSeconds = ttlSeconds;
         this.signingAlgorithmSupplier = signingAlgorithmSupplier;
         this.verificationAlgorithmSupplier = verificationAlgorithmSupplier;
         this.leewaySeconds = leewaySeconds != null && leewaySeconds > 0 ? leewaySeconds : 0;
+        this.keyIdPredicate = keyIdPredicate;
+        this.payloadPredicates = payloadPredicates;
     }
 
     /**
@@ -77,14 +89,17 @@ public class JwtManager {
      * @return the decoded token if it is valid
      * @throws JWTVerificationException if the token is invalid
      */
-    public DecodedJWT verify(@NonNull String token) {
+    public DecodedJWT verify(@NonNull String token) throws JWTVerificationException {
         DecodedJWT decodedJWT = JWT_INSTANCE.decodeJwt(token);
         log.info("decoded jti: {} for sub: {}", decodedJWT.getId(), decodedJWT.getSubject());
         JWTs.verifyExpiresAt(decodedJWT, leewaySeconds);
-        JWTVerifier jwtVerifier = JWT.require(verificationAlgorithmSupplier.getAlgorithm(decodedJWT))
-                .acceptLeeway(leewaySeconds)
-                .build();
-        jwtVerifier.verify(decodedJWT);
+        if (keyIdPredicate != null) {
+            JWTs.verifyKeyId(decodedJWT, keyIdPredicate);
+        }
+        Verification verification = JWT.require(verificationAlgorithmSupplier.getAlgorithm(decodedJWT))
+                .acceptLeeway(leewaySeconds);
+        payloadPredicates.forEach(verification::withClaim);
+        verification.build().verify(decodedJWT);
         log.info("verified jti: {} for sub: {}", decodedJWT.getId(), decodedJWT.getSubject());
         return decodedJWT;
     }
