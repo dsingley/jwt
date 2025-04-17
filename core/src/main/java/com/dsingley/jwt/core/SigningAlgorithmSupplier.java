288b5d5d7c6499ecced5c90539f6fbadcaf4bf4a
David Singley <dsingley@vt.edu>
2025-04-17 16:25:04 +0000

    add some javadoc

--- core/src/main/java/com/dsingley/jwt/core/SigningAlgorithmSupplier.java
+++ core/src/main/java/com/dsingley/jwt/core/SigningAlgorithmSupplier.java
@@ -2,6 +2,9 @@ package com.dsingley.jwt.core;
 
 import com.auth0.jwt.algorithms.Algorithm;
 
+/**
+ * A supplier of {@link Algorithm} instances to be used in the Signing process of JSON Web Tokens.
+ */
 public interface SigningAlgorithmSupplier {
 
     Algorithm getAlgorithm();
--- core/src/main/java/com/dsingley/jwt/core/VerificationAlgorithmSupplier.java
+++ core/src/main/java/com/dsingley/jwt/core/VerificationAlgorithmSupplier.java
@@ -3,6 +3,9 @@ package com.dsingley.jwt.core;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
+/**
+ * A supplier of {@link Algorithm} instances to be used in the Verification process of JSON Web Tokens.
+ */
 public interface VerificationAlgorithmSupplier {
 
     Algorithm getAlgorithm(DecodedJWT decodedJWT);
